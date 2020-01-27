ce9d0e60e1ccb1f33424e0c2b3578ac292398626
Rodrigo Saraya Salas <rodrigo.saraya@gestionysistemas.com>
2020-01-27 04:33:10 -0500

    mejoras

--- app/Proyecto.Evaluacion.WCF/App_Code/IService.cs
+++ app/Proyecto.Evaluacion.WCF/App_Code/IService.cs
@@ -17,4 +17,34 @@ public interface IService
 
     [OperationContract]
     int UpdUsuario(Usuario usuario);
+
+    //Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+
+    //[OperationContract]
+    //string GetData(int value);
+
+    //[OperationContract]
+    //CompositeType GetDataUsingDataContract(CompositeType composite);
 }
+
+
+//[DataContract]
+//public class CompositeType
+//{
+//    bool boolValue = true;
+//    string stringValue = "Hello ";
+
+//    [DataMember]
+//    public bool BoolValue
+//    {
+//        get { return boolValue; }
+//        set { boolValue = value; }
+//    }
+
+//    [DataMember]
+//    public string StringValue
+//    {
+//        get { return stringValue; }
+//        set { stringValue = value; }
+//    }
+//}
--- app/Proyecto.Evaluacion.WCF/App_Code/Service.cs
+++ app/Proyecto.Evaluacion.WCF/App_Code/Service.cs
@@ -25,4 +25,23 @@ public class Service : IService
         var connection = new Connection();
         return connection.UpdUsuario(usuario);
     }
+
+
+    //public string GetData(int value)
+    //{
+    //    return string.Format("You entered: {0}", value);
+    //}
+
+    //public CompositeType GetDataUsingDataContract(CompositeType composite)
+    //{
+    //    if (composite == null)
+    //    {
+    //        throw new ArgumentNullException("composite");
+    //    }
+    //    if (composite.BoolValue)
+    //    {
+    //        composite.StringValue += "Suffix";
+    //    }
+    //    return composite;
+    //}
 }
